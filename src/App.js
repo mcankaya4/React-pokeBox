54c889c7accdd7e94e7feb086a5535ad40325385
mcankaya4 <mcankaya4@gmail.com>
2025-04-25 00:07:34 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,130 +1,55 @@
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Main from "./component/Main";
+import { useEffect, useState } from "react";
+import { useNumber } from "./useNumber";
+import Loader from "./component/Loader";
+
 function App() {
+  const [numbers, regenerate] = useNumber();
+  const [pokes, setPokes] = useState([]);
+  const [myPokes, setMyPokes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function handleSelected(poke) {
+    if (myPokes.length > 5) return;
+    setMyPokes((cur) => [...cur, poke]);
+    regenerate();
+  }
+
+  useEffect(() => {
+    async function fetchPokes() {
+      setLoading(true);
+      try {
+        const responses = await Promise.all(
+          numbers.map((num) =>
+            fetch(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res) =>
+              res.json(),
+            ),
+          ),
+        );
+        setPokes(responses);
+      } catch (error) {
+        console.error("Hata:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (numbers.length) {
+      fetchPokes();
+    }
+  }, [numbers]);
+
   return (
     <div className="app">
-      <h1 className="title">
-        poke<span>Box</span>
-      </h1>
-
-      <div className="card-box">
-        <div className="card">
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/76.svg"
-            alt="butterfly"
-          />
-          <div className="info">
-            <h3>Golem</h3>
-            <div className="stats">
-              <p>
-                ❤️hp <span>45</span>
-              </p>
-              <p>
-                🗡️attack <span>45</span>
-              </p>
-              <p>
-                🛡️defense <span>45</span>
-              </p>
-              <p>
-                🏃🏻‍♂️speed <span>45</span>
-              </p>
-            </div>
-          </div>
-          <span className="type poison">Poison</span>
-        </div>
-        <div className="card">
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/17.svg"
-            alt="butterfly"
-          />
-          <div className="info">
-            <h3>Pidgeotto</h3>
-            <div className="stats">
-              <p>
-                ❤️hp <span>45</span>
-              </p>
-              <p>
-                🗡️attack <span>45</span>
-              </p>
-              <p>
-                🛡️defense <span>45</span>
-              </p>
-              <p>
-                🏃🏻‍♂️speed <span>45</span>
-              </p>
-            </div>
-            <span className="type grass">Grass</span>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/24.svg"
-            alt="butterfly"
-          />
-          <div className="info">
-            <h3>Arbok</h3>
-            <div className="stats">
-              <p>
-                ❤️hp <span>45</span>
-              </p>
-              <p>
-                🗡️attack <span>45</span>
-              </p>
-              <p>
-                🛡️defense <span>45</span>
-              </p>
-              <p>
-                🏃🏻‍♂️speed <span>45</span>
-              </p>
-            </div>
-            <span className="type electric">Electric</span>
-          </div>
-        </div>
-      </div>
-
-      <div className="footer-box">
-        <div className="cart-open">
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/57.svg"
-            alt="butterfly"
-          />
-          <div className="info">
-            <h3>Hitmonli</h3>
-            <div className="stats">
-              <p>
-                ❤️hp <span>45</span>
-              </p>
-              <p>
-                🗡️attack <span>45</span>
-              </p>
-              <p>
-                🛡️defense <span>45</span>
-              </p>
-              <p>
-                🏃🏻‍♂️speed <span>45</span>
-              </p>
-            </div>
-          </div>
-          <span className="type poison">Poison</span>
-        </div>
-        <div className="card-slot">
-          <div className="card-cover"></div>
-        </div>
-        <div className="card-slot">
-          {" "}
-          <div className="card-cover"></div>
-        </div>
-        <div className="card-slot">
-          {" "}
-          <div className="card-cover"></div>
-        </div>
-        <div className="card-slot">
-          {" "}
-          <div className="card-cover"></div>
-        </div>
-        <div className="card-slot">
-          {" "}
-          <div className="card-cover"></div>
-        </div>
-      </div>
+      <Header />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Main pokes={pokes} onSelected={handleSelected} />
+      )}
+      <Footer myPokes={myPokes} />
     </div>
   );
 }
